dae7a306d6c0b1a4ef5ac2178c0e64ade54645fe
sarkerjahiduzzaman <78559065+sarkerjahiduzzaman@users.noreply.github.com>
2022-08-27 23:22:46 +0600

    connect user json link for email conditon

--- js/main.js
+++ js/main.js
@@ -1,18 +1,51 @@
+
+
+
 document.getElementById('login-btn').addEventListener('click',function(){
-    const getElement=document.getElementById('email');
+     const getElement=document.getElementById('email');
     const getEmail=getElement.value;
     console.log(getEmail)
-    const getElement2=document.getElementById('password');
-    const getPassword=getElement2.value;
-    if(getEmail=='jahid@gmail.com'&&getPassword=='2451999')
-    {
-        location.href = "bank.html";
-    }
-    else 
-    {
-        alert('Enter an valid email and password')
-    }
+     const getElement2=document.getElementById('password');
+     const getPassword=getElement2.value;
+
+    
+
+     function datafetch(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res=> res.json())
+        .then(data=>display1(data))
+    
+        
+        }
+        datafetch()
+        
+        function display1(data){
+            for(const email of data)
+            {
+
+               // console.log(email.email)
+            
+                if(getEmail==email.email && getPassword=='2451999')
+                {
+                    console.log(email)
+                   location.href = "bank.html";
+                   break;
+               }
+               
+           
+        
+        }
+       /* else 
+        {
+            alert('Enter an valid email and password')
+        }*/
+
+        
+    
+
     getElement.value='';
     getElement2.value='';
-})
+ }
 
+
+})
